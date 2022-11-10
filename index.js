1bf370de8af5dd404c298127383ec92dbc93f50d
Samad <samadafzal1@g.ucla.edu>
2022-11-10 13:28:26 -0800

    fixed glitches

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-//modules and Globals
-
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+  res.send('Hello world home page')
+})
+
+app.get('/', (req, res) => {
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Not Found!</h1>')
+  res.send('404 page')
 })
 
 app.listen(process.env.PORT)
-
 
