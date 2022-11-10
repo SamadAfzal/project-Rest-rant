1778d8a22610268aff475ad256c2221941dee012
Samad <samadafzal1@g.ucla.edu>
2022-11-10 13:51:42 -0800

    adding 404 page view

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
+
 const app = express()
 
+// Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
-
-app.get('/', (req, res) => {
-  res.send('Hello world home page')
-})
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 app.get('*', (req, res) => {
-  res.send('404 page')
+    res.render('error404')
 })
 
+// Listen for Connections
 app.listen(process.env.PORT)
-
