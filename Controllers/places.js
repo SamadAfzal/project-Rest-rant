e6d015291c101692c4ec8f3ad16cb1cd0f09fceb
Samad <samadafzal1@g.ucla.edu>
2022-11-13 12:17:14 -0800

    Delete fuunctionality implemented

--- Controllers/places.js
+++ Controllers/places.js
@@ -16,10 +16,10 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id] })
-
+    res.render('places/show', { place: places[id], id })
   }
 })
+
 
 
 
@@ -45,5 +45,20 @@ router.post('/', (req, res) => {
 router.get('/', (req, res) => {
     res.render('places/index', {places})
 })
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,25 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
-          </main>
+            <h1>{ data.place.name }</h1>
+                <h2>Rating</h2>
+                    <h3>Currently Unrated</h3>
+                <h2>Comments</h2>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>     
+                <form method="POST" action=""> 
+                <button type="submit" className="btn btn-danger">
+                    Delete
+                </button>
+                </form>     
+
+            </main>
         </Def>
     )
 }
 
 module.exports = show
+
