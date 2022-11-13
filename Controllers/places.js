b7cc24e03b9cec141d68c82315893b67966e288d
Samad <samadafzal1@g.ucla.edu>
2022-11-13 09:47:50 -0800

    adding adn rendering a new view

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,18 +1,23 @@
 const router = require('express').Router()
 
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: 'public/images/piotr-miazga-WBX-ZLr8P7I-unsplash.jpg'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/nathan-dumlao-2z3MOB3kfJU-unsplash.jpg'
       }]
       
     res.render('places/index', {places})
--- views/home.jsx
+++ views/home.jsx
@@ -1,19 +1,24 @@
 const React = require('react')
-const Def = require('./default.jsx')
+const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-              <a href="/places">
-                <button className="btn-primary">Places Page</button>
-              </a>
-
-          </main>
-      </Def>
-    )
-  }
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="/Users/samad/Rest-rant/public/images/goodeats-yqr-DRZHzm-uAaM-unsplash.jpg" alt="Ramen" />
+                </div>
+                Photo by <a href="https://unsplash.com/@goodeats_yqr?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">GoodEats YQR</a> on <a href="https://unsplash.com/s/photos/free-ramen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
   
+                
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
+    )
+}
 
 module.exports = home
+
