379f7b3e32f0e096820c7642a21ca5ea3305396d
Samad <samadafzal1@g.ucla.edu>
2022-11-29 17:32:45 -0800

    added changes

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,7 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-
+    let comments = (
+        <h3 className="inactive">
+          No comments yet!
+        </h3>
+    )
+        if (data.place.comments.length) {
+            comments = data.place.comments.map(c => {
+              return (
+                <div className="border">
+                  <h2 className="rant">{c.rant ? '😡' : '😻'}</h2>
+                  <h4>{c.content}</h4>
+                  <h3>
+                    <stong>- {c.author}</stong>
+                  </h3>
+                  <h4>Rating: {c.stars}</h4>
+                </div>
+              )
+            })
+          }
     return (
         <Def>
           <main>
@@ -15,13 +33,51 @@ function show (data) {
                  <h3>{data.place.showEstablished()}</h3>
                  <h4>Serving {data.place.cuisines}</h4>
             <div className="col-sm-6"></div>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>     
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">Edit</a>     
+                <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
                     <button type="submit" className="btn btn-danger">
                         Delete
                     </button>
                 </form> 
                 </div>
+                <h2>Comments</h2>
+        {comments}
+        <h2>Got Your Own Rant or Rave?</h2>
+        <form method="POST" action={`${data.place.id}/comment`}>
+          <div className="form-group">
+            <label htmlFor="content">Content</label>
+            <input className="form-control" id="content" name="content" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="author">Author</label>
+            <input className="form-control" id="author" name="author" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="stars">Star Rating</label>
+            <input
+              type="number"
+              className="form-control"
+              id="stars"
+              name="stars"
+              step={0.5}
+              value={5}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="rant">Rant?</label>
+            <input
+              type="checkbox"
+              id="rant"
+              name="rant"
+              value={true}
+            />
+          </div>
+          <input
+            className="btn btn-primary"
+            type="submit"
+            value="Add Comment"
+          />
+        </form>
             </main>
         </Def>
     )
